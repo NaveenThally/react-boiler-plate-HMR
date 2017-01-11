0e7c39c012865276eabb6e02ae0c1ca6e69ce090
NaveenThally <naveen0242009@gmail.com>
2017-01-11 19:17:40 +0530

    Updated Files

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <p> Simple React Boiler Plate  with Hot Module Reloader.</p>
+      </div>
+    );
+  }
+
+}
+
+export default App;
